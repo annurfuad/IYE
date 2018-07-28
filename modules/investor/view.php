dad33561e340d9fceda8c07e681d45131e136582
annurfuad <41727797+annurfuad@users.noreply.github.com>
2018-07-28 10:55:28 +0700

    Add files via upload

---
+++ modules/investor/view.php
@@ -0,0 +1,23 @@
+<!-- Content Header (Page header) -->
+<section class="content-header">
+  <h1>
+    <i class="fa fa-user icon-title"></i> Investor
+
+  </h1>
+
+</section>
+
+<!-- Main content -->
+<section class="content">
+  <div class="row">
+    <div class="col-md-12">
+
+      <div class="box box-primary">
+        <div class="box-body">
+          <img src="assets/img/investor.png"  width="100%" height="100%">
+        </div><!-- /.box-body -->
+      </div><!-- /.box -->
+    </div><!--/.col -->
+  </div>   <!-- /.row -->
+
+</section><!-- /.content -->
